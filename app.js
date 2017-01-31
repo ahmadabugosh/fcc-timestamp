8bc972ce4e8cb6ff5ee03681fa5f4bd0b2c020bc
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-01-31 19:15:36 +0400

    Setup basic API request from url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name:'Ahmad' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+
+	var data= {time:req.params.time};
+  res.json(data);
 });
 
 module.exports = router;
