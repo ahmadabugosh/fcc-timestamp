8b4e36b2e1189855a160e62ce6247ce44e2edbc4
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-02-01 04:38:11 +0400

    Added unix timestamp convertor function

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,28 @@ router.get('/', function(req, res) {
 
 router.get('/:time', function(req, res) {
 
-	var data= {time:req.params.time};
+	function toUnix(data)
+	{
+		var date = new Date(data *1000);
+		var monthsLookup =['January', 'February','March','April', 'May', 'June', 'July', 'August','September', 'October', 'November', 'December'];
+		
+		var month=monthsLookup[date.getMonth()];
+		var day= date.getDate();
+		var year=date.getFullYear();
+
+		var finalDate=month + ' ' + day + ', ' + year;
+		return finalDate;
+
+	}
+
+if(!isNaN(req.params.time))
+{
+	var result= toUnix(req.params.time);
+	var data= {unix:req.params.time, natural:result};
   res.json(data);
+
+}
+	
 });
 
 module.exports = router;
